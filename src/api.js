f51bd1e9e267828f7d43536b945203d42ae39b88
Elke <r0830699@student.thomasmore.be>
2021-12-14 08:57:15 +0100

    test api

--- src/api.js
+++ src/api.js
@@ -8,10 +8,6 @@ const router = require('./routes');
 const app = express();
 
 app.use(express.json());
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    next();
-});
 
 app.use('/.netlify/functions/api', router);
 
